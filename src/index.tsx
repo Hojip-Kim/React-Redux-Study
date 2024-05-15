29a9a81e83975671035cf2cf79f04b28cd691209
Hojip-Kim <rlaghwlq12@naver.com>
2024-05-15 21:49:31 +0900

    feat : redux logger 생성 및 Provider 사용

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from './reducer';
+import { Provider } from 'react-redux';
+import { thunk } from 'redux-thunk';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const loggerMiddleware = (store: any) => (next: any) => (action: any) => {
+  console.log('store', store);
+  console.log('action', action);
+  next(action);
+};
+
+const middleware = applyMiddleware(thunk, loggerMiddleware);
+
+const store = createStore(rootReducer, undefined, middleware);
+
+const render = () =>
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  );
+
+render();
+store.subscribe(render);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
