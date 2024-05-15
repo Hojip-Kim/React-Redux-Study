7d5343289fa61547631e5cdac0a0b7a24fda5f84
Hojip-Kim <rlaghwlq12@naver.com>
2024-05-15 21:50:44 +0900

    feat : 전역 상태관리툴 테스트를 위한 App.tsx 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from './reducer';
+import { getchPosts } from './actions/posts';
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+}
 
 function App() {
+  const dispatch = useDispatch();
+  const counter = useSelector((state: RootState) => state.counter);
+  const todos: string[] = useSelector((state: RootState) => state.todos);
+
+  const posts: Post[] = useSelector((state: RootState) => state.posts);
+
+  const [todoValue, setTodoValue] = useState('');
+
+  useEffect(() => {
+    dispatch(getchPosts());
+  }, [dispatch]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  };
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); // 페이지 리프레쉬 막아줌
+    dispatch({ type: 'ADD_TODO', text: todoValue });
+    setTodoValue('');
+  };
+
+  const handleIncrement = () => {
+    dispatch({ type: 'INCREMENT' });
+  };
+
+  const handleDecrement = () => {
+    dispatch({ type: 'DECREMENT' });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Clicked: {counter} times
+      <button onClick={handleIncrement}>+</button>
+      <button onClick={handleDecrement}>-</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+      <form onSubmit={addTodo}>
+        <input type="text" value={todoValue} onChange={handleChange} />
+        <input type="submit" />
+      </form>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
